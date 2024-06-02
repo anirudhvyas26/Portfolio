4de3a3a5eecaae46421ec5e22d407dab1eb48326
Anirudh <anirudhvyas26@gmail.com>
2024-06-02 17:58:37 +0530

    Portfolio

---
+++ animated-portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+import "./app.scss"
+import Navbar from "./components/Navbar";
+const App = () => {
+  return <div>
+   
+    <section >
+      <Navbar/>
+    </section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>Contact</section>
+  </div>;
+};
+
+export default App;
---
+++ animated-portfolio/src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/*sidebar*/}
+      <div className="wrapper">
+        <span>Anirudh </span>
+        <div className="social">
+          {/* <a href="#"> <img src="/instagram.png" alt="" /></a>
+          <a href="#"> <img src="youtube.png" alt=""/></a>
+          <a href="#"> <img src="dribble.png" alt="" /></a> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
