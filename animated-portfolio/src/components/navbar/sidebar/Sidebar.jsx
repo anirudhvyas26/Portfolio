39761726e5bba3e702958286b7682a4887aadef2
Anirudh <anirudhvyas26@gmail.com>
2024-06-09 00:09:39 +0530

    Portfolio Links importing issue in sidebar

--- animated-portfolio/src/components/navbar/sidebar/Sidebar.jsx
+++ animated-portfolio/src/components/navbar/sidebar/Sidebar.jsx
@@ -1,13 +1,20 @@
 import "./sidebar.scss";
 import ToggleButton from "./toggleButton/ToggleButton";
 import Links from "./links/Links";
+import { useState } from "react";
 const Sidebar = () => {
+  const [open,setOpen] = useState(false)
+  const variants ={
+open:
+closed:
+  
+  }
   return (
-    <div className="sidebar">
+    < motion.div className="sidebar">
 <div className="bg">
-    Links
+    <Links/>
     </div> 
-    <ToggleButton/>
+    <ToggleButton setopen={setOpen}/>
      </div>
   ) 
 };
