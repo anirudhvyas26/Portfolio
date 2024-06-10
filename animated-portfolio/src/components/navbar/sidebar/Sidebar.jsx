38e23c2d13baa55b5210ef909b62cbbebc057b56
Anirudh <anirudhvyas26@gmail.com>
2024-06-10 18:23:00 +0530

    TOggle button error

--- animated-portfolio/src/components/navbar/sidebar/Sidebar.jsx
+++ animated-portfolio/src/components/navbar/sidebar/Sidebar.jsx
@@ -2,21 +2,35 @@ import "./sidebar.scss";
 import ToggleButton from "./toggleButton/ToggleButton";
 import Links from "./links/Links";
 import { useState } from "react";
+import { delay, motion } from "framer-motion";
 const Sidebar = () => {
-  const [open,setOpen] = useState(false)
-  const variants ={
-open:
-closed:
-  
-  }
+  const [open, setOpen] = useState(false);
+  const variants = {
+    open: {
+      clipPath:"circle(1200px at 50px 50px)",
+      transition:{
+        type: "spring",
+        stiffness: 20,
+      },
+    },
+    closed: {
+      clipPath: "circle(30px at 50px 50px)",
+      transition: {
+        delay: 0.5,
+        type: "spring",
+        stiffness: 400,
+        damping: 40,
+      },
+    },
+  };
   return (
-    < motion.div className="sidebar">
-<div className="bg">
-    <Links/>
-    </div> 
-    <ToggleButton setopen={setOpen}/>
-     </div>
-  ) 
+    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+      <motion.div className="bg" variants={variants}>
+       Links 
+      </motion.div>
+      <ToggleButton setopen={setOpen} />
+    </motion.div>
+  );
 };
 
-export default Sidebar
+export default Sidebar;
