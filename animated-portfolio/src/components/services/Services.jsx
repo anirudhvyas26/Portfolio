0a0146b6b3d627a917f7e63ccbe600e5e76e7438
Anirudh <anirudhvyas26@gmail.com>
2024-06-17 21:55:52 +0530

    what we do section1

--- animated-portfolio/src/components/services/Services.jsx
+++ animated-portfolio/src/components/services/Services.jsx
@@ -1,17 +1,32 @@
 import "./services.scss";
 import { motion } from "framer-motion";
-
+ const variants={
+  initial:{ x:-500,
+    y:100,
+    opacity:0
+  },
+  animate:{
+    x:0,
+    opacity:1,
+    y:0,
+    transition:{
+      duration:1,
+      stagerChildren:0.1,
+      
+    }
+  }
+ }
 const Services = () => {
   return (
-    <motion.div className="Services">
-      <motion.div className="textContainer">
+    <motion.div className="Services" variants={variants} initial="initial" animate="animate">
+      <motion.div className="textContainer" variants={variants}>
         <p>
           “No loss should be more regrettable to us than losing our time, <br />{" "}
-          for it's irretrievable.”
+          for its irretrievable.”
         </p>
         <hr />
       </motion.div>
-      <motion.div className="titleContainer">
+      <motion.div className="titleContainer" variants={variants}>
         <div className="title">
           <img src="/people.webp" alt="" />
           <h2>
@@ -26,7 +41,7 @@ const Services = () => {
         </div>
       </motion.div>
 
-      <motion.div className="listContainer">
+      <motion.div className="listContainer" variants={variants}>
         <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
             <h2>Wtriting</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, totam aliquid quos molestiae, maxime deserunt perferendis exercitationem nobis ut autem eligendi quis ipsum repellat voluptatum minus! Ipsum laborum quam placeat.</p>
