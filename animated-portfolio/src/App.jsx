1687834a0d107cc2cd59ea387448cc5b9d6e3228
Anirudh <anirudhvyas26@gmail.com>
2024-06-21 02:27:05 +0530

    emailjs pending

--- animated-portfolio/src/App.jsx
+++ animated-portfolio/src/App.jsx
@@ -1,25 +1,36 @@
 //import Test from "./Test";
-import "./app.scss"
+import "./app.scss";
 import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/navbar/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 const App = () => {
-  return <div>
-   
-    <section  id="HomePage">
-      <Navbar/>
-      <Hero/>
-    </section>
-    <section id="Services"><Parallax type="Services"/></section>
-    <section ><Services/></section>
-    <section id="Portfolio"><Parallax type="portfolio"/></section>
-    <Portfolio/>
-<section id="Contact"><Contact/></section>
-{/*<Test><><Test</>*/}
-  </div>;
+  return (
+    <div>
+      <Cursor/>
+      <section id="HomePage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">
+        <Parallax type="Services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+      {/*<Test><><Test</>*/}
+    </div>
+  );
 };
 
 export default App;
