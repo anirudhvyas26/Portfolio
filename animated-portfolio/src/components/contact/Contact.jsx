376715ea7d94f942fd4d4a706bba3f9959bc0dff
Anirudh <anirudhvyas26@gmail.com>
2024-06-20 12:49:54 +0530

    form

--- animated-portfolio/src/components/contact/Contact.jsx
+++ animated-portfolio/src/components/contact/Contact.jsx
@@ -17,7 +17,14 @@ const Contact = () => {
         <h2>Phone</h2>
         <span>9829584974</span>
       </div>
-      <div className="formContainer"></div>
+      <div className="formContainer">
+<form >
+    <input type="text" required placeholder="Name" />
+    <input type="email" required placeholder="Email" />
+    <textarea rows={8} placeholder = "Message" />
+    <button>Submit</button>
+    </form >
+      </div>
        </div>
     </div>
   )
