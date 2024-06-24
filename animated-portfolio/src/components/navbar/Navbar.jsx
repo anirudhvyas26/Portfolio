77aafb79a55536c937356a66b042bda935044bc1
Gaurav Bora <gauravbora2008@gmail.com>
2024-06-24 20:31:01 +0530

    big issues

--- animated-portfolio/src/components/navbar/Navbar.jsx
+++ animated-portfolio/src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <div className="wrapper">
         <motion.span initial={{opacity:0, scale:0.5}}animate={{opacity:1, scale:1 }} transition={{duration:0.5}}>Anirudh </motion.span>
         <div className="social">
-          <a href="#"> <img src="/instagram.png" alt="" /></a>
+          <a href="#"> <img src="instagram.png" alt="" /></a>
           <a href="#"> <img src="youtube.png" alt=""/></a>{/*git hub */}
           <a href="#"> <img src="dribbble.png" alt="" /></a>
         </div>
