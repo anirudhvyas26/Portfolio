74bd95a06f19fcd56e58024812f633c55cc34a83
Anirudh <anirudhvyas26@gmail.com>
2024-06-29 13:01:31 +0530

    about section

--- animated-portfolio/src/App.jsx
+++ animated-portfolio/src/App.jsx
@@ -7,6 +7,7 @@ import Hero from "./components/navbar/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
+import About from "./components/about/About";
 const App = () => {
   return (
     <div>
@@ -28,9 +29,11 @@ const App = () => {
       <section id="Contact">
         <Contact />
       </section>
-      <section>
-        about
+      <About>
+      <section id="About">
+        About
       </section>
+      </About>
       {/*<Test><><Test</>*/}
     </div>
   );
---
+++ animated-portfolio/src/components/about/About.jsx
@@ -0,0 +1,24 @@
+import "./about.scss";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="imageContainer">
+        <img src="\anirudh.jpg" alt="" />
+      </div>
+      <h1>
+        I am Anirudh, a Webdeveloper interested in exploring the codes of life
+      </h1>
+      <p>
+     
+        Music, Writing, Coding and Engineering are the routes I use to
+        experience and express myself on this road.{" "}
+      </p>
+      <div className="about-action">
+        <div className="about-resume">My Resume</div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
