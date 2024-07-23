cd9bfa42ec6f4c80653d35d4e7cc8c237c71b6c9
Anirudh <anirudhvyas26@gmail.com>
2024-07-23 15:18:21 +0530

    vercel file

--- animated-portfolio/src/components/cursor/Cursor.jsx
+++ animated-portfolio/src/components/cursor/Cursor.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./cursor.scss";
-import { motion } from "framer-motion";
+import { easeOut, motion } from "framer-motion";
 
 const Cursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
@@ -17,7 +17,13 @@ const Cursor = () => {
   return (
     <motion.div
       className="cursor"
-      animate={{ x: position.x+10, y: position.y+20 }}
+      animate={{ x: position.x+20, y: position.y+20 }}
+      transition={{
+        type: "tween",
+        duration: "0",
+        ease:"easeOut",
+        damping:"30",
+      }}
     ></motion.div>
   );
 };
